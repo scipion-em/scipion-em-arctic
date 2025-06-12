864de36ae39f7cb9d40f2859ae6e7e56428a122b
JorMaister <jorgejjs86@gmail.com>
2025-06-12 11:20:01 +0200

    Advanced

--- arctic/protocols/protocol_remove_corrupted_tilts.py
+++ arctic/protocols/protocol_remove_corrupted_tilts.py
@@ -25,11 +25,14 @@
 # *
 # **************************************************************************
 import logging
+from collections import OrderedDict
 from enum import Enum
+from typing import Union
 
 from pwem.protocols import EMProtocol
 from pyworkflow import BETA
-from pyworkflow.protocol import PointerParam, BooleanParam, LEVEL_ADVANCED
+from pyworkflow.object import Pointer
+from pyworkflow.protocol import PointerParam, BooleanParam, LEVEL_ADVANCED, EnumParam, STEPS_PARALLEL
 from pyworkflow.utils import Message
 from tomo.objects import SetOfTiltSeries
 
@@ -37,6 +40,19 @@ logger = logging.getLogger(__name__)
 # Form variables
 IN_TS_SET = 'inTsSet'
 RE_STACK_OUT_TS = 'reStackOutTsSet'
+MODEL = 'model'
+
+# Models for binary classification
+SWIN_LARGE = 'swin large'
+SWIN_TINY = 'swin tiny'
+EFFICIENT_NET_B3 = 'EfficientNet_b3'
+RESNET_50 = 'resnet50'
+
+modelsBinaryCl = OrderedDict()
+modelsBinaryCl[SWIN_LARGE] = 0,
+modelsBinaryCl[SWIN_TINY] = 1,
+modelsBinaryCl[EFFICIENT_NET_B3] = 2,
+modelsBinaryCl[RESNET_50] = 3
 
 
 class arcticOutputs(Enum):
@@ -52,6 +68,7 @@ class ProtArcticRemoveCorruptedTilts(EMProtocol):
     _label = 'automated removal of corrupted tilts'
     _devStatus = BETA
     _possibleOutputs = arcticOutputs
+    stepsExecutionMode = STEPS_PARALLEL
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -66,7 +83,41 @@ class ProtArcticRemoveCorruptedTilts(EMProtocol):
         form.addParam(RE_STACK_OUT_TS, BooleanParam,
                       default=False,
                       label='Re-stack the output tilt-series?',
-                      expertLevel=LEVEL_ADVANCED,
                       help='If set to No, the output tilt-series will be mark the bad tilt-images at metadata '
                            'level. Otherwise, a new binary will be generated for each tilt-series containing only '
                            'the good tilt-images.')
+        form.addParam(MODEL, EnumParam,
+                      display=EnumParam.DISPLAY_HLIST,
+                      choices=list(modelsBinaryCl.keys()),
+                      label='Choose a pre-trained model',
+                      expertLevel=LEVEL_ADVANCED,
+                      help='Pre-trained model that will be used for classifying images.')
+        form.addParallelSection(threads=1, mpi=0)
+
+    # --------------------------- INSERT steps functions ----------------------
+    def _insertAllSteps(self):
+        self._initialize()
+        closeSetStepDeps = []
+
+    # -------------------------- STEPS functions ------------------------------
+    def _initialize(self):
+        tsSet = self._getTsSet()
+
+    # --------------------------- UTILS functions -----------------------------
+    def _getFormAttrib(self, attribName) -> Union[Pointer, BooleanParam, EnumParam, None]:
+        return getattr(self, attribName, None)
+
+    def _getFormValue(self, attribName: str) -> Union[bool, int, None]:
+        attrib = self._getFormAttrib(attribName)
+        return attrib if attrib is None else attrib.get()
+
+    def _getTsSet(self,
+                  returnPointer: bool = False) -> Union[SetOfTiltSeries, None]:
+        tsPointer = self._getFormAttrib(IN_TS_SET)
+        return tsPointer if returnPointer else tsPointer.get()
+
+    # def _getFormValue(self,
+    #                    attribName: str,
+    #                    returnPointer: bool = False) -> Union[SetOfTiltSeries, bool, int, None]:
+    #     attribVal = getattr(self, attribName, None)
+    #     return attribVal if returnPointer else attribVal.get()
